3d79b586054169a4ba7bb3d2e8ddb527ee5c0415
AngryShift <99751059+AngryShift@users.noreply.github.com>
2023-03-23 01:00:28 -0400

    V 0.0.1
    First Commit. Added basic folders, background and sprite(singular).

---
+++ Assets/Scripts/ScreenRipple.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Coded By William
+public class ScreenRipple : MonoBehaviour
+{
+    public float ZoomChange;
+    public float SmoothChange;
+    public float Minsize, Maxsize;
+
+    private Camera cam;
+
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+        ZoomChange = 0.25f;
+        SmoothChange = 1f;
+        Minsize = 4.85f;
+        Maxsize = 4.95f;
+
+    }
+
+    private void Update()
+    {
+
+
+        cam.orthographicSize -= ZoomChange * Time.deltaTime * SmoothChange;
+
+        if (cam.orthographicSize < Minsize)
+        {
+            ZoomChange = -0.05f;
+        }
+        if (cam.orthographicSize > Maxsize)
+        {
+            ZoomChange = 0.05f;
+        }
+
+        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, Minsize, Maxsize);
+    }
+}
