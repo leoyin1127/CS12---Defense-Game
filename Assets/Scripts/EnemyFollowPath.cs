5b30319a89688285aa546afc7a72d63efd37bdc9
cincindzz <cindz.487@gmail.com>
2023-03-27 02:30:25 -0400

    path

---
+++ Assets/Scripts/EnemyFollowPath.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollowPath : MonoBehaviour
+{
+    public float speed;
+    private Waypoints Wpoints;
+    private int waypointIndex;
+
+    void Start()
+    {
+        Wpoints = GameObject.FindGameObjectWithTag("Waypoints").GetComponent<Waypoints>();
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, Wpoints.waypoints[waypointIndex].position, speed * Time.deltaTime);
+        if(Vector2.Distance(transform.position, Wpoints.waypoints[waypointIndex].position) < 0.1f)
+        {
+            waypointIndex++;
+        }
+
+    }
+}
