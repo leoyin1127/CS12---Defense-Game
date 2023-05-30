fc559f1fdc703852c8f65cf04a6c777a3bb0391f
Leo Yin <71328966+leoyin1127@users.noreply.github.com>
2023-05-30 13:55:22 -0400

    Revert "update"
    This reverts commit b03a20b352ba08404195f24173d17aac060fe551.

--- Assets/Scripts/StoreManager.cs
+++ Assets/Scripts/StoreManager.cs
@@ -1,6 +1,8 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 
 public class StoreManager : MonoBehaviour
 {
@@ -16,45 +18,51 @@ public class StoreManager : MonoBehaviour
     public Button addExtraGun1Button;
     public Button addExtraGun2Button;
 
-    public Text moneyText;
+    private int Upgrade_max = 10;
+    private int current_speed_level = 0;
+    private int current_damage_level = 0;
+    private int current_fireRate_level = 0;
+    private int current_laserDamage_level = 0; 
+    private int current_laserFireRate_level = 0;
+
+    // Percentage increase for upgrades
+    public float speedUpgradePercent = 0.05f;
+    public float damageUpgradePercent = 0.05f;
+    public float fireRateUpgradePercent = 0.05f;
+    public float laserDamageUpgradePercent = 0.05f;
+    public float laserFireRateUpgradePercent = 0.05f;
+
+    // Prices for upgrades and items
+    public float speedUpgradePrice = 50f;
+    public float damageUpgradePrice = 75f;
+    public float fireRateUpgradePrice = 100f;
+    public float addLaserGunPrice = 150f;
+    public float laserDamageUpgradePrice = 125f;
+    public float laserFireRateUpgradePrice = 150f;
+    public float addExtraGun1Price = 500f;
+    public float addExtraGun2Price = 500f;
+
 
     public float playerMoney = 0;
 
-    private List<Upgrade> upgrades;
+    public Text moneyText;
+
+    public bool ButtonDown = false;
 
     void Start()
     {
-        InitializeUpgrades();
-        InitializeButtons();
+        speedUpgradeButton.onClick.AddListener(() => UpgradeSpeed());
+        damageUpgradeButton.onClick.AddListener(() => UpgradeDamage());
+        fireRateUpgradeButton.onClick.AddListener(() => UpgradeFireRate());
+        addLaserGunButton.onClick.AddListener(() => AddLaserGun());
+        laserDamageUpgradeButton.onClick.AddListener(() => UpgradeLaserDamage());
+        laserFireRateUpgradeButton.onClick.AddListener(() => UpgradeLaserFireRate());
+        addExtraGun1Button.onClick.AddListener(() => AddExtraGun1());
+        addExtraGun2Button.onClick.AddListener(() => AddExtraGun2());
 
         EnemyValues.OnEnemyKilled += IncreasePlayerMoney;
         UpdateMoneyText();
     }
-
-    private void InitializeUpgrades()
-    {
-        upgrades = new List<Upgrade>
-        {
-            new SpeedUpgrade(),
-            new DamageUpgrade(),
-            new FireRateUpgrade(),
-            new LaserDamageUpgrade(),
-            new LaserFireRateUpgrade(),
-        };
-    }
-
-    private void InitializeButtons()
-    {
-        speedUpgradeButton.onClick.AddListener(() => UpgradeItem("Speed"));
-        damageUpgradeButton.onClick.AddListener(() => UpgradeItem("Damage"));
-        fireRateUpgradeButton.onClick.AddListener(() => UpgradeItem("FireRate"));
-        addLaserGunButton.onClick.AddListener(() => AddLaserGun());
-        laserDamageUpgradeButton.onClick.AddListener(() => UpgradeItem("LaserDamage"));
-        laserFireRateUpgradeButton.onClick.AddListener(() => UpgradeItem("LaserFireRate"));
-        addExtraGun1Button.onClick.AddListener(() => AddExtraGun1());
-        addExtraGun2Button.onClick.AddListener(() => AddExtraGun2());
-    }
-
     private void UpdateMoneyText()
     {
         moneyText.text = $"Money: {playerMoney}";
@@ -70,38 +78,121 @@ public class StoreManager : MonoBehaviour
         storePanel.SetActive(false);
     }
 
-    public void UpgradeItem(string upgradeName)
+    void UpgradeSpeed()
     {
-        foreach (Upgrade upgrade in upgrades)
+        if (playerMoney >= speedUpgradePrice && current_speed_level < Upgrade_max)
         {
-            if (upgrade.Name == upgradeName)
-            {
-                if (playerMoney >= upgrade.Price && upgrade.CanUpgrade())
-                {
-                    playerMoney -= upgrade.Price;
-                    upgrade.ApplyUpgrade();
-                    UpdateMoneyText();
-                }
-                break;
-            }
+            current_speed_level++;
+            playerMoney -= speedUpgradePrice;
+            // Implement the logic to increase the player's moving speed by percentage
+            // Update the Ship speed variable in MainChar script.
+            MainChar mainChar = FindObjectOfType<MainChar>();
+            mainChar.ShipInstance.Speed *= (1 + speedUpgradePercent);
+            Debug.Log("speed up");
+            UpdateMoneyText();
+        }
+    }
+
+
+    void UpgradeDamage()
+    {
+        if (playerMoney >= damageUpgradePrice && current_damage_level < Upgrade_max)
+        {
+            current_damage_level++;
+            playerMoney -= damageUpgradePrice;
+            // Implement the logic to increase the player's damage by percentage.
+            
+            WeaponController weaponController = FindObjectOfType<WeaponController>();
+            Debug.Log($"player damge: {weaponController.damage}");
+            weaponController.damage *= (1 + damageUpgradePercent);
+            
+            UpdateMoneyText();
+        }
+    }
+
+    void UpgradeFireRate()
+    {
+        if (playerMoney >= fireRateUpgradePrice && current_fireRate_level < Upgrade_max)
+        {
+            current_fireRate_level++;
+            playerMoney -= fireRateUpgradePrice;
+            UpdateMoneyText();
+            // Implement the logic to increase the player's firing speed by percentage.
+            // Assuming you have a WeaponController script with a fireRate variable:
+            ManualWeapon manualWeapon = FindObjectOfType<ManualWeapon>();
+            manualWeapon.cooldown *= (1 - fireRateUpgradePercent);
         }
     }
 
     void AddLaserGun()
     {
-        // Implement the logic to add a laser gun into the scene.
+
+        if (playerMoney >= addLaserGunPrice)
+        {
+            playerMoney -= addLaserGunPrice;
+            UpdateMoneyText();
+            ButtonDown = true;
+            print("11");
+            // Implement the logic to add a laser gun into the scene.
+        }
+    }
+
+    void UpgradeLaserDamage()
+    {
+        if (playerMoney >= laserDamageUpgradePrice && current_laserDamage_level < Upgrade_max)
+        {
+            current_laserDamage_level++;
+            playerMoney -= laserDamageUpgradePrice;
+            UpdateMoneyText();
+            // Implement the logic to increase the laser gun's damage.
+            // LaserGunController[] laserGunControllers = FindObjectsOfType<LaserGunController>();
+            // foreach (LaserGunController laserGunController in laserGunControllers)
+            // {
+            //     laserGunController.damage *= (1 + laserDamageUpgradePercent);
+            // }
+        }
+    }
+
+    void UpgradeLaserFireRate()
+    {
+        if (playerMoney >= laserFireRateUpgradePrice && current_laserFireRate_level < Upgrade_max)
+        {
+            current_laserFireRate_level++;
+            playerMoney -= laserFireRateUpgradePrice;
+            UpdateMoneyText();
+            // Implement the logic to increase the laser gun's firing speed by percentage.
+            // Assuming you have a LaserGunController script with a fireRate variable:
+            // LaserGunController[] laserGunControllers = FindObjectsOfType<LaserGunController>();
+            // foreach (LaserGunController laserGunController in laserGunControllers)
+            // {
+            //     laserGunController.fireRate *= (1 + laserFireRateUpgradePercent);
+            // }
+        }
     }
 
     void AddExtraGun1()
     {
-        // Implement the logic to add the first extra gun into the scene.
-        topGun.SetActive(true);
+        if (playerMoney >= addExtraGun1Price)
+        {
+            playerMoney -= addExtraGun1Price;
+            UpdateMoneyText();
+            addExtraGun1Price *= 1.1f;
+            topGun.SetActive(true);
+            // Implement the logic to add the first extra gun into the scene.
+        }
     }
 
     void AddExtraGun2()
     {
-        // Implement the logic to add the second extra gun into the scene.
-        bottomGun.SetActive(true);
+        if (playerMoney >= addExtraGun2Price)
+        {
+            playerMoney -= addExtraGun2Price;
+            UpdateMoneyText();
+            addExtraGun2Price *= 1.1f;
+
+            // Implement the logic to add the second extra gun into the scene.
+            bottomGun.SetActive(true);
+        }
     }
 
     void OnDestroy()
@@ -113,5 +204,7 @@ public class StoreManager : MonoBehaviour
     {
         playerMoney += cashAmount;
         UpdateMoneyText();
+        Debug.LogWarning($"Player money increased by {cashAmount}");
     }
+
 }
